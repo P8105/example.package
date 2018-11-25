62c6c853b2ec84108197bd925e6e8df6fd04005d
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2018-11-25 15:42:10 -0500

    add documentation

--- R/simulate_nruns.R
+++ R/simulate_nruns.R
@@ -1,6 +1,17 @@
+#' Repeat simulations
+#'
+#' @param n_runs
+#' @param n sample size
+#' @param beta0 intercept
+#' @param beta1 slope
+#'
+#' @return tibble with `n_runs` rows
+#' @export
+#'
+#' @examples
 simulate_nruns = function(n_runs = 100, n, beta0 = 2, beta1 = 3) {
-  
-  rerun(n_runs, sim_regression(n, beta0, beta1)) %>% 
+
+  rerun(n_runs, sim_regression(n, beta0, beta1)) %>%
     bind_rows()
-  
+
 }
